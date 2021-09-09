900185866fb639041682948829c7b21c5e4e9ead
AndrewP3 <andrewpowell097@gmail.com>
2021-09-09 16:49:42 -0700

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div>
+      <nav></nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
