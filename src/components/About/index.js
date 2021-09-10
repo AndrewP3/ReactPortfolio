1067ecc2ae46bed4beb8f1086ce6b3cdae4bf9dc
AndrewP3 <andrewpowell097@gmail.com>
2021-09-10 14:13:57 -0700

    6th commit

---
+++ src/components/About/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import avatarImage from "";
+
+function About() {
+  return (
+    <section className="flex-row">
+      <h1 id="about" className="flex-row">
+        About Me{" "}
+      </h1>
+      <img src={avatarImage} alt="Andrew Powell III" className="avatar" />
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+    </section>
+  );
+}
+
+export default About;
