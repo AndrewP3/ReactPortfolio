4806b33d85352a95228c970b178870282f8f4ba9
AndrewP3 <andrewpowell097@gmail.com>
2021-09-09 17:03:21 -0700

    3rd commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "../../portfolio-react-main/src/reportWebVitals";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+reportWebVitals();
