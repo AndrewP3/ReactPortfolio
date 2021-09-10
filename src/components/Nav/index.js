a3168d6fd6e3f659957a35772d4e2c81638795c5
AndrewP3 <andrewpowell097@gmail.com>
2021-09-10 14:33:22 -0700

    7th commit

---
+++ src/components/Nav/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="flex-row">
+      <h2 className="navBar">
+        <a href="/">Andrew Powell III</a>
+      </h2>
+      <ul className="mx-2 flex-row">
+        <li>
+          <a href="#about" className="mx-2">
+            <button>About Me</button>
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio" className="mx-2">
+            <button>Portfolio</button>
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="mx-2">
+            <button>Contact</button>
+          </a>
+        </li>
+        <li>
+          <a href="#resume" className="mx-2">
+            <button>Resume</button>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
